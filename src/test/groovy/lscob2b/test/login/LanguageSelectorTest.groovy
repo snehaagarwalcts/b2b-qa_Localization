195d11f349a3ef879344c774274baace91ee8616
dshah1992 <dipen.shah@sap.com>
2014-12-02 17:44:10 -0500

    removed somethings that weren't needed

--- src/test/groovy/lscob2b/test/login/LanguageSelectorTest.groovy
+++ src/test/groovy/lscob2b/test/login/LanguageSelectorTest.groovy
@@ -1,34 +1,27 @@
 package lscob2b.test.login
 
 import lscob2b.modules.MasterTemplate
-import lscob2b.pages.HomePage
 import lscob2b.pages.LoginPage;
 import spock.lang.Stepwise;
 import geb.spock.GebReportingSpec
 import geb.spock.GebSpec
+
 import org.openqa.selenium.support.ui.Select
 
 
 class LanguageSelectorTest extends GebReportingSpec {
-
-	String defaultPassword = "12341234"
-	String levisUser = "robert.moris@monsoon.com"
-	String dockersUser = "deno.rota@fashion-world.com"
-	String multibrandUser = "joseph.hall@city-apparel.com"
 
 	def setup(){
 
 		to LoginPage
 	}
 
-	def "Log in with Swedish as your default language"(){
+	def "Change web page language from English to Swedish"(){
 
 		when: "Select Swedish as language"
 		langSelector = 'sv'
 
 		then: "Check that login page language is Swedish"
-		pageheading == "Återkommande kund"
-
+		pageheading == "Återkommande Kund"
 	}
-	
 }
