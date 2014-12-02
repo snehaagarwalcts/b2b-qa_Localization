0b30c1dfb21d6da4362c6110e02c963da28d8605
dshah1992 <dipen.shah@sap.com>
2014-12-02 12:32:40 -0500

    Reformat

--- src/test/groovy/lscob2b/pages/LoginPage.groovy
+++ src/test/groovy/lscob2b/pages/LoginPage.groovy
@@ -1,66 +1,61 @@
 package lscob2b.pages
 
 import geb.Page
-import lscob2b.modules.MasterTemplate //added this cause of an error "Log in with Swedish as your default language(lscob2b.test.login.LanguageSelectorTest): Unable to resolve masterTemplate as content for LoginPage, or as a property on its Navigator context. Is masterTemplate a class you forgot to import?"
-
+import lscob2b.modules.MasterTemplate
 
 class LoginPage extends Page {
 
-    static url = ""
+	static url = ""
 
-    static at = {
-        title == "LSCO B2B Site"
-    }
+	static at = { title == "LSCO B2B Site" }
 
-    static content = {
-        // login form
-        usernameInput(wait: true) { $("#j_username") }
-        passwordInput { $("#j_password") }
-        loginButton { $("#loginForm button") }
+	static content = {
+		// login form
+		usernameInput(wait: true) { $("#j_username") }
+		passwordInput { $("#j_password") }
+		loginButton { $("#loginForm button") }
 
-        // some error messages
-        globalMessages { $("#globalMessages") }
-        errorMessage { $("#globalMessages div.negative") }
-        errorMessageText { errorMessage.text().trim() }
+		// some error messages
+		globalMessages { $("#globalMessages") }
+		errorMessage { $("#globalMessages div.negative") }
+		errorMessageText { errorMessage.text().trim() }
 
-        // forgotten password
-        forgottenPasswordDialog(required: false) { $("#cboxLoadedContent") }
-        forgottenPasswordDialogVisible(required: false) { forgottenPasswordDialog.present && forgottenPasswordDialog.displayed }
-        forgottenYourPasswordButton { $("#loginForm a") }
-        closeForgottenPasswordButton (required: false) { $('#cboxClose') }
-        sendEmailButton(required: false) { $("#forgottenPwdForm button") }
-        emailAddress { $("#forgottenPwdForm input") }
-		   
-		  // language selector
-		  langSelector { $("#lang-selector") }
-		  pageheading { $("div.title_holder h2").text() }
-    }
+		// forgotten password
+		forgottenPasswordDialog(required: false) { $("#cboxLoadedContent") }
+		forgottenPasswordDialogVisible(required: false) { forgottenPasswordDialog.present && forgottenPasswordDialog.displayed }
+		forgottenYourPasswordButton { $("#loginForm a") }
+		closeForgottenPasswordButton (required: false) { $('#cboxClose') }
+		sendEmailButton(required: false) { $("#forgottenPwdForm button") }
+		emailAddress { $("#forgottenPwdForm input") }
 
-    def doLogin(String username, String password) {
-        usernameInput = username
-        passwordInput = password
-        loginButton.click()
-    }
+		// language selector
+		langSelector { $("#lang-selector") }
+		pageheading { $("div.title_holder h2").text() }
+	}
 
-    def openForgottenPasswordDialog() {
-        forgottenYourPasswordButton.click()
+	def doLogin(String username, String password) {
+		usernameInput = username
+		passwordInput = password
+		loginButton.click()
+	}
 
-        waitFor {
-            forgottenPasswordDialogVisible
-            emailAddress
-        }
-    }
+	def openForgottenPasswordDialog() {
+		forgottenYourPasswordButton.click()
 
-    def closeForgottenPasswordDialog() {
-        closeForgottenPasswordButton.click()
+		waitFor {
+			forgottenPasswordDialogVisible
+			emailAddress
+		}
+	}
 
-        waitFor {
-            !forgottenPasswordDialogVisible
-        }
-    }
+	def closeForgottenPasswordDialog() {
+		closeForgottenPasswordButton.click()
 
-    def sendForgottenPasswordEmail(String email) {
-        emailAddress = email
-        sendEmailButton.click()
-    }
+		waitFor { !forgottenPasswordDialogVisible }
+	}
+
+	def sendForgottenPasswordEmail(String email) {
+		emailAddress = email
+		sendEmailButton.click()
+	}
 }
