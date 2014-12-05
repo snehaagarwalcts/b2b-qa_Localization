f8330d9a88bc73d74d7236311807e81e9e15deff
Dilip Mamidela <dilip.mamidela@sap.com>
2014-12-05 18:22:22 +0530

    Fixed and completed both the stories

--- src/test/groovy/lscob2b/test/myaccount/MyAccountBreadcrumbTest.groovy
+++ src/test/groovy/lscob2b/test/myaccount/MyAccountBreadcrumbTest.groovy
@@ -2,27 +2,65 @@ package lscob2b.test.myaccount
 
 import geb.spock.GebReportingSpec
 import lscob2b.pages.LoginPage
+import lscob2b.pages.HomePage
 import lscob2b.pages.MyAccountPage
+import spock.lang.Stepwise
 
+@Stepwise
 /**
- * Created by i303936 on 12/3/14.
+ * Created by i303936 on 12/3/14 , added by I065970 on 12/5/14
  */
 class MyAccountBreadcrumbTest extends GebReportingSpec {
+	
+	static String defaultPassword = "12341234"
 
-    def setupSpec() {
-        to LoginPage
-        doLogin("robert.moris@monsoon.com", "12341234")
-    }
-
-    def "Check breadcrumb trail"() {
-        when: "At my account page"
-
-        to MyAccountPage
-
-        then: "Home and my account breadcrumbs are present"
-
-        masterTemplate.breadcrumbExistsByUrl("/lscob2bstorefront/lscob2b/en/")
-        masterTemplate.breadcrumbExistsByUrl("/lscob2bstorefront/lscob2b/en/my-account")
-        masterTemplate.breadcrumbIsActiveByUrl("/lscob2bstorefront/lscob2b/en/my-account")
-    }
+	static String levisUser = "robert.moris@monsoon.com"
+	static String dockersUser = "deno.rota@fashion-world.com"
+	static String multibrandUser = "joseph.hall@city-apparel.com"
+		
+	def setup() {
+		to LoginPage
+	}
+	
+   	def login(String username) {
+		doLogin(username, defaultPassword)
+	}
+	
+	def cleanup() {
+		masterTemplate.doLogout()
+	}
+	
+	def "Login ,go to My account Page and Check breadcrumb"() { // tests the login itself without worrying about rights
+		when: "Login as selected user"
+		
+				login (user)
+		
+				then: "We should see correct logo "
+		
+				at HomePage
+				
+				when: "at my homepage"
+				
+				to MyAccountPage
+				
+				then:"check bread crumbs "
+				
+				masterTemplate.breadcrumbExistsByUrl("/lscob2bstorefront/lscob2b/en/")
+				masterTemplate.breadcrumbExistsByUrl("/lscob2bstorefront/lscob2b/en/my-account")
+				masterTemplate.breadcrumbIsActiveByUrl("/lscob2bstorefront/lscob2b/en/my-account")
+				println masterTemplate.breadcrumbs.size() // added by I065970 on 12/4/14
+				assert masterTemplate.breadcrumbs.size() == 2
+		
+			//	!themeForm
+		
+			//	logoAltTag == themeSpecificAltTag
+		
+				where:
+		
+				user		| themeSpecificAltTag
+				levisUser	| "Levis Strauss & Company"
+				dockersUser	| "Levis Strauss & Company" // TODO this should be different for Dockers, not yet implemented
+				multibrandUser | "Levis Strauss & Company"
+				}
+	
 }
--- src/test/groovy/lscob2b/test/myaccount/MyAccountTest.groovy
+++ src/test/groovy/lscob2b/test/myaccount/MyAccountTest.groovy
@@ -1,98 +1,59 @@
-//Created by I065970 on 12/2/14
 package lscob2b.test.myaccount
 
 import geb.spock.GebReportingSpec
-import lscob2b.pages.HomePage
 import lscob2b.pages.LoginPage
+import lscob2b.pages.HomePage
 import lscob2b.pages.MyAccountPage
 import spock.lang.Stepwise
 
 @Stepwise
+
+//Created by I065970 on 12/2/14 and modified by I065970 on 12/5/14
+
 class MyAccountTest extends GebReportingSpec {
-    String defaultPassword = "12341234"
-    String dockersUser = "deno.rota@fashion-world.com"
-    String levisUser = "robert.moris@monsoon.com"
-    String multibrandUser = "joseph.hall@city-apparel.com"
+	
+	static String defaultPassword = "12341234"
 
-    def setup() {
-        to LoginPage
-    }
-
-    def cleanup() {
-        masterTemplate.doLogout()
-    }
-
-
-    def login(String username) {
-        doLogin(username, defaultPassword)
-    }
-
-    def "Login as Dockers and go to My Account"() {
-        when: "Logging in as a Dockers only customer"
-
-        login(dockersUser)
-
-        then: "We should see Dockers logo"
-
-        at HomePage
-
-        when: "at home page"
-
-        masterTemplate.doMyaccount()
-
-        then: "we should see my account"
-
-        at MyAccountPage
-
-        !themeForm
-
-        logoAltTag == "Levis Strauss & Company" // TODO This text should change depending on Levi's or Dockers.
-        // As of now it's always Levi's even for Dockers customer
-
-    }
-
-    def "Login as levis user and go to My Account"() {
-        when: "Logging in as a Levis only customer"
-
-        login(levisUser)
-
-        then: "We should see Levis logo"
-
-        at HomePage
-
-        when: "at home page"
-
-        masterTemplate.doMyaccount()
-
-        then: "we should see my account"
-
-        at MyAccountPage
-
-
-        !themeForm
-
-        logoAltTag == "Levis Strauss & Company" // TODO This text should change depending on Levi's or Dockers.
-        // As of now it's always Levi's even for Dockers customer
-
-    }
-
-    def "Login as mutilbrand and go to My Account"() {
-        when: "Logging in as a Multibrand only customer"
-
-        login(multibrandUser)
-
-        then: "We should see Levi's logo and theme selector"
-
-        at HomePage
-
-        when: "at home page"
-
-        masterTemplate.doMyaccount()
-
-        then: "we should see my account"
-
-        at MyAccountPage
-
-    }
-
+	static String levisUser = "robert.moris@monsoon.com"
+	static String dockersUser = "deno.rota@fashion-world.com"
+	static String multibrandUser = "joseph.hall@city-apparel.com"
+		
+	def setup() {
+		to LoginPage
+	}
+	
+	   def login(String username) {
+		doLogin(username, defaultPassword)
+	}
+	
+	def cleanup() {
+		masterTemplate.doLogout()
+	}
+	
+	def "Login and click on My account Page"() { // tests the login itself without worrying about rights
+		when: "Login as selected user"
+		
+				login (user)
+		
+				then: "We should see correct logo "
+		
+				at HomePage
+				
+				when: "at my homepage"
+				
+				to MyAccountPage
+				
+				then:"at my accounts page"
+			//	!themeForm
+		
+			//	logoAltTag == themeSpecificAltTag
+		
+				where:
+		
+				user		| themeSpecificAltTag
+				levisUser	| "Levis Strauss & Company"
+				dockersUser	| "Levis Strauss & Company" // TODO this should be different for Dockers, not yet implemented
+				multibrandUser | "Levis Strauss & Company"
+				}
+	
 }
