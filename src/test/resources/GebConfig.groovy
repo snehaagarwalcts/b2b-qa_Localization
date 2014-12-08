d6ca576cab0d08ec6620d55ebfb38e9cefe7cb30
dshah1992 <dipen.shah@sap.com>
2014-12-08 09:32:17 -0500

    added integration001, and qa001 URLS

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -8,34 +8,41 @@ def sysProps = System.getProperties()
 def sauceBrowser = sysProps.get "geb.sauce.browser"
 
 if (sauceBrowser) {
-    driver = {
-        def env = System.getenv()
-        def username = sysProps.get "geb.sauce.user"
-        def accessKey = sysProps.get  "geb.sauce.access.key"
-        def platform = env.get "SELENIUM_PLATFORM"
-        def version = env.get "SELENIUM_VERSION"
-        def browserName = env.get "SELENIUM_BROWSER"
-        def capabilities = [platform: platform, version: version, browserName: browserName]
-        new SauceLabsDriverFactory().create("", username, accessKey, capabilities)
-    }
+	driver = {
+		def env = System.getenv()
+		def username = sysProps.get "geb.sauce.user"
+		def accessKey = sysProps.get  "geb.sauce.access.key"
+		def platform = env.get "SELENIUM_PLATFORM"
+		def version = env.get "SELENIUM_VERSION"
+		def browserName = env.get "SELENIUM_BROWSER"
+		def capabilities = [platform: platform, version: version, browserName: browserName]
+		new SauceLabsDriverFactory().create("", username, accessKey, capabilities)
+	}
 } else {
-    driver = { new FirefoxDriver() }
+	driver = { new FirefoxDriver() }
 }
 
 localHost = "lscob2b.local:9001"
-intHost = "b2bint-001-store-000.lsco-b2b.com:9001"
-qaHost = "b2bqa-000-web-000.lsco-b2b.com"
+intHost000 = "b2bint-000-store-000.lsco-b2b.com:9001"
+intHost001 = "b2bint-001-store-000.lsco-b2b.com:9001"
+qaHost000 = "b2bqa-000-web-000.lsco-b2b.com"
+qaHost001 = "b2bqa-001-web-000.lsco-b2b.com"
 
 baseUrl = "http://" + localHost + "/lscob2bstorefront/"
 
 environments {
-    integration {
-        baseUrl = "http://" + intHost + "/lscob2bstorefront/"
-    }
-
-    qa {
-        baseUrl = "http://" + qaHost
-    }
+	integration000 {
+		baseUrl = "http://" + intHost000 + "/lscob2bstorefront/"
+	}
+	integration001 {
+		baseUrl = "http://" + intHost001 + "/lscob2bstorefront/"
+	}
+	qa000 { 
+		baseUrl = "http://" + qaHost000
+	}
+	qa001 {
+		 baseUrl = "http://" + qaHost001 
+	}
 }
 
 reportsDir = new File("target/geb-reports")
