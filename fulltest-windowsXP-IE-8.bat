331740dbc4d8b70acea9c5d55eb65371f2c14859
dshah1992 <dipen.shah@sap.com>
2015-02-02 10:49:47 -0500

    Sauce labs tunnel setting

---
+++ fulltest-windowsXP-IE-8.bat
@@ -0,0 +1,6 @@
+set SELENIUM_PLATFORM=Windows XP
+set SELENIUM_VERSION=8
+set SELENIUM_BROWSER=Internet explorer
+
+echo "Starting test on %SELENIUM_PLATFORM% / %SELENIUM_BROWSER% / %SELENIUM_VERSION%"
+mvn test-compile test -DargLine="-Dgeb.sauce.browser=1 -Dgeb.sauce.user=dshah1992 -Dgeb.sauce.access.key=2620798f-5186-4a1c-b1ea-3b5f5f2ae882"
