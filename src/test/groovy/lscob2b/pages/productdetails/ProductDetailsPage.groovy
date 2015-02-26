7661057110af47609685d3d3fa8b3e154e8a85ce
dshah1992 <dipen.shah@sap.com>
2015-02-26 13:49:08 -0500

    not part of your brand assortment test

--- src/test/groovy/lscob2b/pages/productdetails/ProductDetailsPage.groovy
+++ src/test/groovy/lscob2b/pages/productdetails/ProductDetailsPage.groovy
@@ -1,7 +1,3 @@
-
-
-
-
 package lscob2b.pages.productdetails
 
 import geb.Page
@@ -21,39 +17,37 @@ class ProductDetailsPage extends Page {
 	}
 
 	static content = {
-		
+
 		masterTemplate { module MasterTemplate }
-		
+
 		sizingGrid { module SizingGridModule}		//TODO remove it [CHECK SIMONE]
-		
+
 		sizingTable { module SizingTableModule, $("div.single_grid_three_dimensions") }
-		
+
 		checkOut { module CheckOutModule}
-		
+
 		upSelling(required: false) { module SellModule, $("div.up-sell") }
-		
+
 		crossSelling(required: false) { module SellModule, $("div.cross-sell") }
-		
+
 		buyStack { module PDPBuyStackModule, $("div.pdp-buystack") }
-		
+
 		wholesalePriceText { $("div.wholesale-price > span").text()}
 		recommendedRetailPriceText { $("div.recommended-retail-price > span").text()}
-//		orderQuantity{ $(".grid_three_dimensions").find("tr > td > .sku-quantity",0) }
-		recommendedRetailPriceValue {
-			parsePrice(recommendedRetailPriceText)
-		}
-		wholesalePriceValue {
-			parsePrice(wholesalePriceText)
-		}
-		
+		//		orderQuantity{ $(".grid_three_dimensions").find("tr > td > .sku-quantity",0) }
+		recommendedRetailPriceValue { parsePrice(recommendedRetailPriceText) }
+		wholesalePriceValue { parsePrice(wholesalePriceText) }
+
 		//OverLay
-		
+
 		overlayWaitList { $("div.popup_box") }
-		
+
 		overlayTable { overlayWaitList.find("table.grid_three_dimensions") }
-		
+
 		overlayButtonAdd { overlayWaitList.find("a#add_to_waitlist_button") }
-		
+
+		notYourBrandAssortmentProduct { $('.productListItem').find("a", href: endsWith('/00501-0101')) }
+
 	}
 
 	def recommendedRetailPriceExist(){
@@ -72,29 +66,29 @@ class ProductDetailsPage extends Page {
 		String s = priceText.find(numberRegex).replace(",","").replace(".","")
 		Double.parseDouble(s.substring(0, s.length()-2) + "." + s.substring(s.length()-2))
 	}
-	
+
 	def addOrderQuantity(String quantity){
 		sizingTable.addLimitedStockQuantity(quantity.toInteger())
 	}
-	
+
 	def findLinkColor(String href) {
 		$("ul.color-swatches").find("a",href:endsWith(href),0)
 	}
-	
+
 	def addOutOfStockQuantityToWaitList(int quantity) {
 		waitFor { sizingGrid.displayed }
 		waitFor { sizingGrid.buttonNotifyMe.displayed }
-		
+
 		sizingGrid.buttonNotifyMe.click()
-		
+
 		waitFor { overlayWaitList.displayed }
-		
+
 		waitFor { overlayTable.displayed }
-		
+
 		overlayTable.find("td.Red",0).find("input.sku-quantity").value(quantity)
-		
+
 		overlayButtonAdd.click()
-		
+
 	}
-	
+
 }
