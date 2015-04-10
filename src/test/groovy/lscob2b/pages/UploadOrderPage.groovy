cf5a941e27367f0786e91e894725d3df87f5053e
401685 <401685@PC210247.cts.com>
2015-04-10 19:19:43 +0530

    CODE ADDED - UploadOrderPageTest

---
+++ src/test/groovy/lscob2b/pages/UploadOrderPage.groovy
@@ -0,0 +1,25 @@
+package lscob2b.pages
+
+import lscob2b.modules.MasterTemplate
+import geb.Page
+
+class UploadOrderPage extends Page{
+
+	static url = "/order/upload"
+
+	static at = { waitFor { title == "Upload Order | LSCO B2B Site"} }
+
+	static content = { 
+		
+		masterTemplate {module MasterTemplate} 
+		
+		downloadFileTitle { $('.optionsDownload>h2') }
+		
+		downloadFileLink { $('a.button.btn-txt-red') }
+		
+		uploadFileTitle { $('.optionsUpload>h2') }
+		
+		uploadFileButton { $('div.upload input.button') }
+		
+	}
+}
---
+++ src/test/groovy/lscob2b/test/landingpage/UploadOrderPageTest.groovy
@@ -0,0 +1,40 @@
+package lscob2b.test.landingpage
+
+import lscob2b.data.PageHelper
+import lscob2b.data.UserHelper
+import lscob2b.pages.LoginPage
+import lscob2b.pages.UploadOrderPage;
+import lscob2b.test.data.PropertProviderTest
+
+class UploadOrderPageTest extends PropertProviderTest{
+	
+	def setup() {
+		PageHelper.gotoPageLogout(browser, baseUrl)
+	}
+	
+	def "Verify translations - Women"() {
+		setup:
+		to LoginPage
+		at LoginPage
+		login(user)
+		browser.go(baseUrl + link)
+		
+		when: "at Upload Order Page"
+		at UploadOrderPage
+		
+		then: "click on UPLOAD FILE button"
+		uploadFileButton.click()
+		
+		and: "verify translations of UploadOrderPage"
+		assert masterTemplate.mainContainerLabel.text() == expectedValue("text.order.upload").toUpperCase()
+		assert masterTemplate.alertMessageHeader.text() == expectedValue("text.please.note").toUpperCase()
+		//assert masterTemplate.alertMessage.text() == expectedValue("orderupload.invalid.file.format")
+		assert downloadFileTitle.text() == expectedValue("orderupload.template.download").toUpperCase()
+		assert downloadFileLink.text()- ~/&/ == expectedValue("orderupload.template.download").toUpperCase()
+		assert uploadFileButton.value() == expectedValue("order.upload.file")
+		
+		where:
+		user | link
+		UserHelper.getUser(UserHelper.B2BUNIT_LEVIS, UserHelper.ROLE_SUPER) | "order/upload"
+	}
+}
